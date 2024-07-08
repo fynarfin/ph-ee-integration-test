7a720785a535872e47647527c6b8545ce505049c
kaibalya-fynarfin <kaibalya@fynarfin.io>
2024-07-08 16:50:54 +0530

    solved code quality issues with regards to checkstyle

--- src/test/java/org/mifos/integrationtest/cucumber/stepdef/ValidateChannelDef.java
+++ src/test/java/org/mifos/integrationtest/cucumber/stepdef/ValidateChannelDef.java
@@ -1,5 +1,9 @@
 package org.mifos.integrationtest.cucumber.stepdef;
 
+import static com.google.common.truth.Truth.assertThat;
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.awaitility.Awaitility.await;
+
 import io.cucumber.core.internal.com.fasterxml.jackson.core.JsonProcessingException;
 import io.cucumber.core.internal.com.fasterxml.jackson.databind.JsonNode;
 import io.cucumber.core.internal.com.fasterxml.jackson.databind.ObjectMapper;
@@ -9,6 +13,8 @@ import io.cucumber.java.en.When;
 import io.restassured.RestAssured;
 import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.specification.RequestSpecification;
+import java.util.ArrayList;
+import java.util.List;
 import org.mifos.connector.common.gsma.dto.CustomData;
 import org.mifos.connector.common.gsma.dto.GsmaTransfer;
 import org.mifos.connector.common.gsma.dto.Party;
@@ -18,14 +24,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.ArrayList;
-import java.util.List;
+public class ValidateChannelDef extends BaseStepDef {
 
-import static com.google.common.truth.Truth.assertThat;
-import static java.util.concurrent.TimeUnit.SECONDS;
-import static org.awaitility.Awaitility.await;
-
-public class ValidateChannelDef extends BaseStepDef{
     @Autowired
     ScenarioScopeState scenarioScopeState;
 
@@ -43,7 +43,7 @@ public class ValidateChannelDef extends BaseStepDef{
         try {
             scenarioScopeState.createTransactionChannelRequestBody = json;
         } catch (Exception e) {
-            logger.error("An Exception occurred",e);
+            logger.error("An Exception occurred", e);
         }
     }
 
@@ -126,8 +126,8 @@ public class ValidateChannelDef extends BaseStepDef{
             RequestSpecification requestSpec = Utils.getDefaultSpec();
             scenarioScopeState.tenant = "gorilla";
             scenarioScopeState.response = RestAssured.given(requestSpec).header("Content-Type", "application/json")
-                    .header("Platform-TenantId", scenarioScopeState.tenant)
-                    .baseUri(channelConnectorConfig.channelConnectorContactPoint).body(scenarioScopeState.createTransactionChannelRequestBody).expect()
+                    .header("Platform-TenantId", scenarioScopeState.tenant).baseUri(channelConnectorConfig.channelConnectorContactPoint)
+                    .body(scenarioScopeState.createTransactionChannelRequestBody).expect()
                     .spec(new ResponseSpecBuilder().expectStatusCode(expectedStatus).build()).when()
                     .post(channelConnectorConfig.transferEndpoint).andReturn().asString();
 
@@ -140,14 +140,13 @@ public class ValidateChannelDef extends BaseStepDef{
         await().atMost(awaitMost, SECONDS).pollDelay(pollDelay, SECONDS).pollInterval(pollInterval, SECONDS).untilAsserted(() -> {
             RequestSpecification requestSpec = Utils.getDefaultSpec();
             scenarioScopeState.callbackUrl = "https://webhook.site/3cb8ab56-c03d-4251-9911-520f235da8f5";
-            scenarioScopeState.amsName="mifos";
-            scenarioScopeState.accountHoldingInstitutionId="gorilla";
+            scenarioScopeState.amsName = "mifos";
+            scenarioScopeState.accountHoldingInstitutionId = "gorilla";
             scenarioScopeState.response = RestAssured.given(requestSpec).header("Content-Type", "application/json")
                     .header("accountHoldingInstitutionId", scenarioScopeState.accountHoldingInstitutionId)
-                    .header("amsName", scenarioScopeState.amsName)
-                    .header("X-CallbackURL", scenarioScopeState.callbackUrl)
-                    .baseUri(channelConnectorConfig.channelConnectorContactPoint).body(scenarioScopeState.createGsmaTransferRequestBody).expect()
-                    .spec(new ResponseSpecBuilder().expectStatusCode(expectedStatus).build()).when()
+                    .header("amsName", scenarioScopeState.amsName).header("X-CallbackURL", scenarioScopeState.callbackUrl)
+                    .baseUri(channelConnectorConfig.channelConnectorContactPoint).body(scenarioScopeState.createGsmaTransferRequestBody)
+                    .expect().spec(new ResponseSpecBuilder().expectStatusCode(expectedStatus).build()).when()
                     .post(channelConnectorConfig.gsmaTransactionEndpoint).andReturn().asString();
 
             logger.info("Gsma transaction Response: {}", scenarioScopeState.response);
@@ -160,8 +159,8 @@ public class ValidateChannelDef extends BaseStepDef{
             RequestSpecification requestSpec = Utils.getDefaultSpec();
             scenarioScopeState.tenant = "gorilla";
             scenarioScopeState.response = RestAssured.given(requestSpec).header("Content-Type", "application/json")
-                    .header("Platform-TenantId", scenarioScopeState.tenant)
-                    .baseUri(channelConnectorConfig.channelConnectorContactPoint).body(scenarioScopeState.createTransactionChannelRequestBody).expect()
+                    .header("Platform-TenantId", scenarioScopeState.tenant).baseUri(channelConnectorConfig.channelConnectorContactPoint)
+                    .body(scenarioScopeState.createTransactionChannelRequestBody).expect()
                     .spec(new ResponseSpecBuilder().expectStatusCode(expectedStatus).build()).when()
                     .post(channelConnectorConfig.transferReqEndpoint).andReturn().asString();
 
@@ -196,7 +195,7 @@ public class ValidateChannelDef extends BaseStepDef{
         try {
             scenarioScopeState.createTransactionChannelRequestBody = json;
         } catch (Exception e) {
-            logger.error("An Exception occurred",e);
+            logger.error("An Exception occurred", e);
         }
     }
 
@@ -206,9 +205,9 @@ public class ValidateChannelDef extends BaseStepDef{
             RequestSpecification requestSpec = Utils.getDefaultSpec();
             scenarioScopeState.tenant = "gorilla";
             scenarioScopeState.response = RestAssured.given(requestSpec).header("Content-Type", "application/json")
-                    .header("Platform-TenantId", scenarioScopeState.tenant)
-                    .header("invalid-header", "test")
-                    .baseUri(channelConnectorConfig.channelConnectorContactPoint).body(scenarioScopeState.createTransactionChannelRequestBody).expect()
+                    .header("Platform-TenantId", scenarioScopeState.tenant).header("invalid-header", "test")
+                    .baseUri(channelConnectorConfig.channelConnectorContactPoint)
+                    .body(scenarioScopeState.createTransactionChannelRequestBody).expect()
                     .spec(new ResponseSpecBuilder().expectStatusCode(expectedStatus).build()).when()
                     .post(channelConnectorConfig.transferEndpoint).andReturn().asString();
 
@@ -222,9 +221,9 @@ public class ValidateChannelDef extends BaseStepDef{
             RequestSpecification requestSpec = Utils.getDefaultSpec();
             scenarioScopeState.tenant = "gorilla";
             scenarioScopeState.response = RestAssured.given(requestSpec).header("Content-Type", "application/json")
-                    .header("Platform-TenantId", scenarioScopeState.tenant)
-                    .header("invalid-header", "test")
-                    .baseUri(channelConnectorConfig.channelConnectorContactPoint).body(scenarioScopeState.createTransactionChannelRequestBody).expect()
+                    .header("Platform-TenantId", scenarioScopeState.tenant).header("invalid-header", "test")
+                    .baseUri(channelConnectorConfig.channelConnectorContactPoint)
+                    .body(scenarioScopeState.createTransactionChannelRequestBody).expect()
                     .spec(new ResponseSpecBuilder().expectStatusCode(expectedStatus).build()).when()
                     .post(channelConnectorConfig.transferReqEndpoint).andReturn().asString();
 
@@ -237,14 +236,13 @@ public class ValidateChannelDef extends BaseStepDef{
         await().atMost(awaitMost, SECONDS).pollDelay(pollDelay, SECONDS).pollInterval(pollInterval, SECONDS).untilAsserted(() -> {
             RequestSpecification requestSpec = Utils.getDefaultSpec();
             scenarioScopeState.callbackUrl = "https://webhook.site/3cb8ab56-c03d-4251-9911-520f235da8f5";
-            scenarioScopeState.amsName="mifos";
-            scenarioScopeState.accountHoldingInstitutionId="gorilla";
+            scenarioScopeState.amsName = "mifos";
+            scenarioScopeState.accountHoldingInstitutionId = "gorilla";
             scenarioScopeState.response = RestAssured.given(requestSpec).header("Content-Type", "application/json")
                     .header("accountHoldingInstitutionId", scenarioScopeState.accountHoldingInstitutionId)
-                    .header("amsName", scenarioScopeState.amsName)
-                    .header("X-CallbackURL", scenarioScopeState.callbackUrl)
-                    .header("invalid-header", "test")
-                    .baseUri(channelConnectorConfig.channelConnectorContactPoint).body(scenarioScopeState.createGsmaTransferRequestBody).expect()
+                    .header("amsName", scenarioScopeState.amsName).header("X-CallbackURL", scenarioScopeState.callbackUrl)
+                    .header("invalid-header", "test").baseUri(channelConnectorConfig.channelConnectorContactPoint)
+                    .body(scenarioScopeState.createGsmaTransferRequestBody).expect()
                     .spec(new ResponseSpecBuilder().expectStatusCode(expectedStatus).build()).when()
                     .post(channelConnectorConfig.gsmaTransactionEndpoint).andReturn().asString();
 
@@ -257,7 +255,8 @@ public class ValidateChannelDef extends BaseStepDef{
         await().atMost(awaitMost, SECONDS).pollDelay(pollDelay, SECONDS).pollInterval(pollInterval, SECONDS).untilAsserted(() -> {
             RequestSpecification requestSpec = Utils.getDefaultSpec();
             scenarioScopeState.response = RestAssured.given(requestSpec).header("Content-Type", "application/json")
-                    .baseUri(channelConnectorConfig.channelConnectorContactPoint).body(scenarioScopeState.createTransactionChannelRequestBody).expect()
+                    .baseUri(channelConnectorConfig.channelConnectorContactPoint)
+                    .body(scenarioScopeState.createTransactionChannelRequestBody).expect()
                     .spec(new ResponseSpecBuilder().expectStatusCode(expectedStatus).build()).when()
                     .post(channelConnectorConfig.transferEndpoint).andReturn().asString();
 
@@ -270,7 +269,8 @@ public class ValidateChannelDef extends BaseStepDef{
         await().atMost(awaitMost, SECONDS).pollDelay(pollDelay, SECONDS).pollInterval(pollInterval, SECONDS).untilAsserted(() -> {
             RequestSpecification requestSpec = Utils.getDefaultSpec();
             scenarioScopeState.response = RestAssured.given(requestSpec).header("Content-Type", "application/json")
-                    .baseUri(channelConnectorConfig.channelConnectorContactPoint).body(scenarioScopeState.createTransactionChannelRequestBody).expect()
+                    .baseUri(channelConnectorConfig.channelConnectorContactPoint)
+                    .body(scenarioScopeState.createTransactionChannelRequestBody).expect()
                     .spec(new ResponseSpecBuilder().expectStatusCode(expectedStatus).build()).when()
                     .post(channelConnectorConfig.transferReqEndpoint).andReturn().asString();
 
@@ -283,12 +283,11 @@ public class ValidateChannelDef extends BaseStepDef{
         await().atMost(awaitMost, SECONDS).pollDelay(pollDelay, SECONDS).pollInterval(pollInterval, SECONDS).untilAsserted(() -> {
             RequestSpecification requestSpec = Utils.getDefaultSpec();
             scenarioScopeState.callbackUrl = "https://webhook.site/3cb8ab56-c03d-4251-9911-520f235da8f5";
-            scenarioScopeState.amsName="mifos";
+            scenarioScopeState.amsName = "mifos";
             scenarioScopeState.response = RestAssured.given(requestSpec).header("Content-Type", "application/json")
-                    .header("amsName", scenarioScopeState.amsName)
-                    .header("X-CallbackURL", scenarioScopeState.callbackUrl)
-                    .baseUri(channelConnectorConfig.channelConnectorContactPoint).body(scenarioScopeState.createGsmaTransferRequestBody).expect()
-                    .spec(new ResponseSpecBuilder().expectStatusCode(expectedStatus).build()).when()
+                    .header("amsName", scenarioScopeState.amsName).header("X-CallbackURL", scenarioScopeState.callbackUrl)
+                    .baseUri(channelConnectorConfig.channelConnectorContactPoint).body(scenarioScopeState.createGsmaTransferRequestBody)
+                    .expect().spec(new ResponseSpecBuilder().expectStatusCode(expectedStatus).build()).when()
                     .post(channelConnectorConfig.gsmaTransactionEndpoint).andReturn().asString();
 
             logger.info("GSMA Transaction Response: {}", scenarioScopeState.response);
